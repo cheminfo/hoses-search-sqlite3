cad2e8bb7e6553e878072a5eeed80fac659c1a6e
Luc Patiny <luc@patiny.com>
2025-01-14 14:52:16 +0100

    wip: refactor importXYZ

--- src/import/importXYZ.js
+++ src/import/importXYZ.js
@@ -21,20 +21,35 @@ export async function importXYZ(content, db, options = {}) {
   const stats = { nbEntries: 0 };
   const xyzEntries = splitXYZ(content);
   let counter = 0;
+
+  // create abject that maps columns to algoID
+  const algorithms = [
+    {
+      column: 4,
+      algorithmID: 123,
+    },
+  ];
+
   for (const xyzLines of xyzEntries) {
     const entry = await getXYZEnhancedEntry(xyzLines, options);
     console.log(entry);
-    for (let i = 0; i < entry.atoms.length; i++) {
-      for (let p = 0; p < entry.atoms[i].properties.length; p++) {
-        const property = entry.atoms[i].properties[p];
-        if (!Number.isNaN(property.energy)) {
-          const algorithmID = insertAlgorithm(
-            property.algorithm,
-            property.contact,
-            db,
-          );
-          // Will import the contact if needed at the same time
-          const entryID = insertEntry(entry, algorithmID, db);
+
+    for (const algorithm of algorithms) {
+      const { column, algorithmID } = algorithm;
+
+      const entryID = insertEntry(entry, algorithmID, db);
+
+      for (let i = 0; i < entry.atoms.length; i++) {
+        for (let p = 0; p < entry.atoms[i].properties.length; p++) {
+          const property = entry.atoms[i].properties[p];
+          if (!Number.isNaN(property.energy)) {
+            const algorithmID = insertAlgorithm(
+              property.algorithm,
+              property.contact,
+              db,
+            );
+            // Will import the contact if needed at the same time
+          }
         }
       }
     }
